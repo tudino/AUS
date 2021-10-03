89648bd3bb54296b16a297c485d6098a4374c9ad
aus <aus@aus>
2021-10-03 14:25:27 -0300

    Agrego Juego de cartas

---
+++ Programacion 3/Juego de Cartas/Carta.java
@@ -0,0 +1,11 @@
+public class Carta {
+    private Palos palos = {"ESPADA", "BASTO", "ORO", "COPA"};
+    private Valores valores = {"1", "2","3","4","5","6","7","10","11","12"};
+    private Palo palo = "ESPADA";
+    private Valor valor = "1";
+    public Carta() {
+
+    }
+
+    private CrearMazo()
+}
---
+++ Programacion 3/Juego de Cartas/Mazo.java
@@ -0,0 +1,13 @@
+class Mazo {
+
+    public String lo = 40;
+    
+    public Mazo() {
+        
+    }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Hel");
+    }
+}
